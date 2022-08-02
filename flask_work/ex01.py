254f4c7f87dbd734d197b5fbb1a1663815c80992
hahahaa23 <ghdrud4749@naver.com>
2022-08-02 13:57:40 +0900

    0802

--- flask_work/ex01.py
+++ flask_work/ex01.py
@@ -1,7 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import time
 
+from sklearn.neighbors import KNeighborsClassifier
+
+x_data =np.array([])
+y_data = np.array([])
 
 x_data = np.array([
     #O
@@ -46,13 +51,25 @@ y_data = np.array([
     [1]
 ])
 
+y_data = ['O']*3+['X']*3
+
 labels = ['오', '엑스']
 
+start_time = time.time()
+knclf = KNeighborsClassifier(n_neighbors=1)
+knclf.fit(x_data,y_data)
+end_time = time.time()
+
+print('걸린시간 = ',end_time-start_time)
+pred = knclf.predict(x_data[0].reshape(-1,25))
+print(pred)
+
 for i in range(6):
+    plt.title(f'target = {y_data[i]}')
     plt.imshow(x_data[i].reshape(5,5),cmap='gray_r')
     plt.savefig(f'x_data[{i}]_plt.png')
     # plt.show()
     plt.close()
 
-for i in range(6):
-    cv2.imwrite(f'x_data[{i}]_cv.png',x_data[i].reshape(5,5))
+# for i in range(6):
+#     cv2.imwrite(f'x_data[{i}]_cv.png',x_data[i].reshape(5,5))
--- flask_work/server.py
+++ flask_work/server.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/test")
+@app.route("/KNeighbors")
 def test():
-    return render_template("test.html")
+    return render_template("KNeighbors.html")
 
 if __name__=='__main__':
     app.run(debug=True)
